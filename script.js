3e6984683a0d07bbbe608fe22bebeb54c5a3ec14
DanielMakokha <danielmakokhan@gmail.com>
2023-08-09 06:33:24 +0300

    page redirect

---
+++ script.js
@@ -0,0 +1,8 @@
+function redirect() {
+        setTimeout(function() {
+        window.location = 'https://danielmakokha.netlify.app/';
+    }, 5000); // 5000 milliseconds = 5 seconds
+    
+}
+
+redirect();
